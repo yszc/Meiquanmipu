ff6c9a68e2dae7706afefd459741058f416db19e
Zhijun <chen.zhijun@foxmail.com>
2016-02-01 15:01:40 +0800

    symbol

--- fanjian.php
+++ fanjian.php
@@ -6,3 +6,18 @@ foreach($files as $file){
     echo $cmd."\n";
     shell_exec($cmd);
 }
+
+$replaceList=array(
+    '︻'=>'【',
+    '︼'=>'】',
+    '︵'=>'（',
+    '︶'=>'）',
+    '︽'=>'《',
+    '︾'=>'》',
+    '﹁ '=>'“',
+    '﹂'=>'”',
+);
+foreach($replaceList as $s1=>$s2){
+    $cmd = sprintf("find epub_source/OEBPS/Text | xargs perl -pi -e 's/%s/%s/g'",$s1,$s2);
+    shell_exec($cmd);
+}
